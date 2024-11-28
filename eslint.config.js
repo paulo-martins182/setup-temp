39d6d049c4032401c71ab5bdb7bdaa3ba0909f37
Paulo Martin <156706183+paulo-martin@users.noreply.github.com>
2024-11-28 15:19:38 -0300

    finish base setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,15 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import ts from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import prettier from 'eslint-plugin-prettier';
-import react from 'eslint-plugin-react'; // Certifique-se de importar o plugin 'react'
+import js from '@eslint/js'
+import globals from 'globals'
+import ts from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import prettier from 'eslint-plugin-prettier'
+import prettierConf from 'eslint-config-prettier'
 
 export default [
   {
-    ignores: ['dist', 'node_modules'],
-  },
-  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
@@ -19,30 +17,41 @@ export default [
       parser: tsParser,
       parserOptions: {
         ecmaFeatures: {
-          jsx: true,
-        },
-      },
+          jsx: true
+        }
+      }
     },
-    files: ['**/*.{ts,tsx,js,jsx}'],
     plugins: {
       '@typescript-eslint': ts,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier,
       react,
+      'react-hooks': reactHooks,
+      prettier
     },
     rules: {
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.jsx'] }],
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react/jsx-filename-extension': [
+        'error',
+        { extensions: ['.tsx', '.jsx'] }
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' }
+      ],
       'react/jsx-boolean-value': ['error', 'always'],
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      'prettier/prettier': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto'
+        }
+      ]
     },
     settings: {
       react: {
-        version: 'detect',
-      },
-    },
+        version: 'detect'
+      }
+    }
   },
-];
+  {
+    ignores: ['node_modules', 'dist', 'public']
+  },
+  prettierConf
+]
