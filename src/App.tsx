15a557be99833b759048b6394c596e852ab6b03a
Paulo Martin <156706183+paulo-martin@users.noreply.github.com>
2024-12-02 09:12:45 -0300

    feat: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import '../src/styles/global.css'
+import '../src/styles/index.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
-    <>
-      <h1> Hello World </h1>
-    </>
+    <div className="flex w-full justify-center items-center h-screen flex-col">
+      <h1> Shadcn UI Button</h1>
+
+      <Button>Button</Button>
+    </div>
   )
 }
 
